f1e2238593b941e3c038b1659987e14ac076a4f1
Sean ZO Marciniak <sean@beamery.com>
2018-09-25 11:25:31 +0100

    Got it to a basic working state

--- mailroom/solution/post.go
+++ mailroom/solution/post.go
@@ -1,17 +1,21 @@
 package solution
 
 type post struct {
-  // Add any variables you need here
+	// Add any variables you need here
 }
 
-func (p *post) GetStreetAddress() string {
-  return ""
+func (p post) GetStreetAddress() string {
+	return ""
 }
 
-func (p *post) GetState() string {
-  return ""
+func (p post) GetState() string {
+	return ""
 }
 
-func (p *post) GetPostcode() int {
-  return 0
+func (p post) GetPostcode() int {
+	return 0
+}
+
+func (p post) Valid() bool {
+	return false
 }
